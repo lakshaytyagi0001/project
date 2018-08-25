0b388355f93116cfa924a805142f961fe3744a0b
Lakshay Tyagi <lakshaytyagi0001@gmail.com>
2018-08-25 17:51:30 +0530

    Initial Setup

---
+++ App.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import { Text, AppRegistry } from 'react-native';
+
+const App = () => (
+    <Text>some text</Text>
+  );
+
+AppRegistry.registerComponent('project', () => App);
